33306fd42a8d8d51dd2c03cfd1a8473007eb3771
Your Nameimane <git config --global user.email you@example.com>
2025-05-12 16:02:49 +0100

    cherche

--- covoiturage-femmef/src/app/core/services/voyage.service.ts
+++ covoiturage-femmef/src/app/core/services/voyage.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Voyage } from '../../models/voyage.model'; 
 @Injectable({
   providedIn: 'root'
 })
 export class VoyageService {
+  private apiUrl = 'http://localhost:8080/api/voyages'; // ou autre URL selon ton backend
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  listerVoyages(): Observable<Voyage[]> {
+    return this.http.get<Voyage[]>(this.apiUrl);
+  }
 }
--- covoiturage-femmef/src/app/models/voyage.model.ts
+++ covoiturage-femmef/src/app/models/voyage.model.ts
@@ -2,16 +2,8 @@ export interface Voyage {
   id: number;
   depart: string;
   destination: string;
-  date: string;
-  heure: string;
+  date: string; // format : 'YYYY-MM-DD'
+  heure: string; // format : 'HH:mm'
   prix: number;
   placesDisponibles: number;
-  conductrice: {
-    id: number;
-    nom: string;
-    photo: string;
-    note: number;
-    nombreEvaluations: number;
-  };
-  status: 'ACTIF' | 'COMPLET' | 'TERMINE';
 }
